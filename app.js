846ceb9cebd3f11b28c2997939480413567ff6ef
Tehseen ULLAH <tehseenullah92@gmail.com>
2018-10-02 19:40:09 +0800

    seaching/list all users page done

--- app.js
+++ app.js
@@ -7,22 +7,6 @@ var port = process.env.PORT || 3000;
 const path = require('path');
 var models = require("./models");
 var users = require('./routes/users');
-// const sqlite3 = require('sqlite3').verbose();
-// open database in memory
-// let db = new sqlite3.Database(':memory:', (err) => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log('Connected to the in-memory SQlite database.');
-// });
-//
-// // close the database connection
-// db.close((err) => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log('Close the database connection.');
-// });
 
 models.sequelize.sync().then(function() {
     console.log('connected to database')
@@ -39,6 +23,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use('/users', users);
+app.use('/list', users);
+app.use('/search', users);
 app.get('/', function(req, res){
     console.log('app listening on port: '+port);
 });
--- routes/users.js
+++ routes/users.js
@@ -27,13 +27,42 @@ router.get('/', function(req, res){
       // res.status(200).json(user);
     });
 });
+router.get('/list', function(req, res){
+    User.findAll().then(user => {
+      res.render('list',{
+        usersArray: user,
+        title: ('List All Users'),
+      });
+      // res.status(200).json(user);
+    });
+});
+router.get('/search', function(req, res){
+    User.findAll().then(user => {
+      res.render('search',{
+        usersArray: user,
+        title: ('List All Users'),
+      });
+      // res.status(200).json(user);
+    });
+});
+router.get('/search/:name', function(req, res, next) {
+    var name = req.params.name;
+    User.findAll({name: name}, function (err, users) {
+        if(err) {
+            return res.render('search', {users: null});
+        }else{
+          console.log(users);
+        res.render('search', {usersArray: users});
+        }
+    });
+});
 router.post('/add', function(req, res){
     User.create({
         name: req.body.name,
         email: req.body.email,
     }).then(user => {
       // res.status(200).json(user);
-        res.redirect('/users');
+        res.redirect('/list');
     }).error(err => {
         res.status(405).json('Error has occured');
     });
